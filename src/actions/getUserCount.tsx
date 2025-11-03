bc75d6f096d6fcc1dc3eede770ae84858d3d45a3
Fernando Rodríguez <ferrodest1999@gmail.com>
2025-11-03 11:55:48 +0100

    fix user count endpoint

--- src/actions/getUserCount.tsx
+++ src/actions/getUserCount.tsx
@@ -1,7 +1,7 @@
 import { ApiResponse } from "futbol-in-core/types";
 
 export const getUserCount = async () => {
-  const res = await fetch(`http://192.168.0.19:3000/user/count`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/count`, {
     next: { revalidate: 60 },
   });
   const { data } = (await res.json()) as ApiResponse<number>;
